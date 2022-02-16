ddd79d629b1166b1fea68fde063dad410069245f
nikita.ulcenko <nikita.ulcenko@accenture.com>
2022-02-16 14:32:01 +0200

    lesson 5

--- src/main/java/com/smitter/application/web/controller/MainController.java
+++ src/main/java/com/smitter/application/web/controller/MainController.java
@@ -3,14 +3,14 @@ package com.smitter.application.web.controller;
 import com.smitter.application.domain.Message;
 import com.smitter.application.domain.User;
 import com.smitter.application.repository.MessageRepository;
+import net.bytebuddy.implementation.bytecode.constant.DefaultValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @Controller
 public class MainController {
@@ -18,37 +18,33 @@ public class MainController {
     MessageRepository messageRepository;
 
     @GetMapping("/")
-        public String greeting(Map<String, Object> model){
-            return "greeting";
-        }
-        @GetMapping("/main")
-        public String main(Map<String,Object> model){
-        Iterable<Message>messages = messageRepository.findAll();
-        model.put("messages", messages);
-        return "main";
-        }
-
-        @PostMapping("/main")
-        public String add(
-                @AuthenticationPrincipal User user,
-                @RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag, user);
-        messageRepository.save(message);
-            Iterable<Message>messages = messageRepository.findAll();
-            model.put("messages", messages);
-        return "main";
+    public String greeting(Model model) {
+        return "greeting";
     }
-        @PostMapping("filter")
-        public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
 
+    @GetMapping("/main")
+    public String main(@RequestParam(required = false, defaultValue = "")  String filter, Model model) {
+        Iterable<Message> messages = messageRepository.findAll();
         if (filter != null && !filter.isEmpty())
             messages = messageRepository.findByTag(filter);
         else {
             messages = messageRepository.findAll();
         }
-            model.put("messages", messages);
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
-            }
-        }
+    }
+
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text, @RequestParam String tag, Model model) {
+        Message message = new Message(text, tag, user);
+        messageRepository.save(message);
+        Iterable<Message> messages = messageRepository.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+}
+
 
